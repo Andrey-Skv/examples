2ada5af750f00f4261bf5d8b99dd3d2eb5e70cc2
Dron <epttit@gmail.com>
2022-06-26 20:38:37 +0300

    ав

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите имя ползователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша");
+{
+    Console.WriteLine("Ура, это маша");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
